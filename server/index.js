df6119185f3a63580a21c762556b97d17069b554
kode <kalidadefa@gmail.com>
2023-12-23 07:59:19 +0300

    vercel config

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const Post = require("./models/Post");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://kalidadefa:76VwtAv1Aj3ra9ON@cluster0.wjulwca.mongodb.net/db?retryWrites=true&w=majority"
+);
+
+app.get("/", (req, res) => {
+  res.json("Hello");
+});
+app.post("/posts", (req, res) => {
+  // const {name, email, password} = req.body;
+  // RegisterModel.findOne({email: email})
+  // .then(user => {
+  //     if(user) {
+  //         res.json("Already have an account")
+  //     } else {
+  //         RegisterModel.create({name: name, email: email, password: password})
+  //         .then(result => res.json(result))
+  //         .catch(err => res.json(err))
+  //     }
+  // }).catch(err => res.json(err))
+
+  Post.find()
+    .then((posts) => {
+      res.json(posts);
+    })
+    .catch((err) => res.json(err));
+});
+
+app.listen(3001, () => {
+  console.log("Server is Running");
+});
---
+++ server/models/Post.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    likes: {
+      type: Array,
+      default: [],
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
